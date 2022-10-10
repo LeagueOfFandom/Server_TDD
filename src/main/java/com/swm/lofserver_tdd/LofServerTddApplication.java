5c23b50ff7604ac3e65f883035155524052774de
summit45 <brandy2145@gmail.com>
2022-10-10 14:58:01 +0900

    [feat] base settings

---
+++ src/main/java/com/swm/lofserver_tdd/LofServerTddApplication.java
@@ -0,0 +1,13 @@
+package com.swm.lofserver_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LofServerTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LofServerTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swm/lofserver_tdd/LofServerTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swm.lofserver_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LofServerTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
