39a744d61c5c49dd25020fed9fc02611fea1655a
summit45 <brandy2145@gmail.com>
2022-10-11 17:05:09 +0900

    [feat] 중복회원 검사 코드

--- src/main/java/com/swm/lofserver_tdd/repository/UserRepository.java
+++ src/main/java/com/swm/lofserver_tdd/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.swm.lofserver_tdd.repository;
 import com.swm.lofserver_tdd.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    List<UserEntity> findByEmail(String email);
 }
--- src/main/java/com/swm/lofserver_tdd/service/UserService.java
+++ src/main/java/com/swm/lofserver_tdd/service/UserService.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -16,10 +18,19 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserEntity createUser(UserRequestDto requestDto) {
+        validateDuplicateUser(requestDto);
+
         UserEntity user = new UserEntity(requestDto);
         userRepository.save(user);
-        log.info("등록한 유저 Id: " + user.getId().toString());
+        log.info("등록된 유저 Id: " + user.getId().toString());
         return user;
+    }
+
+    private void validateDuplicateUser(UserRequestDto requestDto){
+        List<UserEntity> findUsers = userRepository.findByEmail(requestDto.getEmail());
+        if(!findUsers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
     }
 
 }
